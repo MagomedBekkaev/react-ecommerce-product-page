0e8845e045bafa38399727d558af36b22101ffc0
Magomed Bekkaev <bekkaev.magomed@gmail.com>
2024-12-05 12:04:57 +0100

    burger menu almost finished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,24 @@ import classes from "./Header.module.css"
 const Header = () => {
     return (
         <nav className={classes.header}>
-            <h1>Pizzeria</h1>
-            <p>Les meilleures pizzas artisanales</p>
-            <img src="/images/logo.svg" alt="SNEAKERS Logo" className={classes.logo} />
+            <div className={classes.topnav}>
+                <img src="/images/logo.svg" alt="SNEAKERS Logo" className={classes.logo} />
+                <img src="/images/icon-cart.svg" alt="Cart Icon" className={classes.cart} />
+                <img src="/images/image-avatar.png" alt="Avatar" className={classes.avatar} />
+                
+                <div className={classes.links}>
+                    <a href="#" className={classes.navLink}>Collections</a>
+                    <a href="#" className={classes.navLink}>Men</a>
+                    <a href="#" className={classes.navLink}>Women</a>
+                    <a href="#" className={classes.navLink}>About</a>
+                    <a href="#" className={classes.navLink}>Contact</a>
+                </div>
+
+                <a href="#" className={classes.menuIcon}>
+                    <img src="/images/icon-menu.svg" alt="Menu Icon" />
+                </a>
+
+            </div>
         </nav>
     )
 }
